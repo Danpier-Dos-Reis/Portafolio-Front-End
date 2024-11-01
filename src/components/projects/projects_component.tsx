141e5adbe101bd9c9363631d5b780893a558d9aa
danpier dos reis <d6nt3_d3m0n@outlook.com>
2024-11-01 13:56:34 -0300

    Projects Page finished

--- src/components/projects/projects_component.tsx
+++ src/components/projects/projects_component.tsx
@@ -5,17 +5,20 @@ function Projects() {
   return (
     <>
       <h1>Projects Content</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, odio?</p>
+      <p className="p_init">Lorem ipsum dolor Animi, odio?</p>
       <section className='projects_section'>
-        <div className="project_image"></div>
+        <div className="project_image">
+          <img src="https://cdn.pixabay.com/photo/2018/12/14/23/57/web-3876081_960_720.jpg" alt="current_image" />
+        </div>
         <div className="project_resume">
-          <a href="/project">Pico Project</a>
+          <a href="/project" className="project_title">Pico Project</a>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, facilis?</p>
           <ul>
             <li>laravel</li>
             <li>typescript</li>
             <li>mysql</li>
           </ul>
-          <button className='btn_project'></button>
+          <a href="/project" className='btn_project'>Look</a>
         </div>
       </section>
     </>
